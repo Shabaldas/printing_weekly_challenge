752358ef0102a553ca8f6685ae2a2e0285df355c
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2022-12-26 18:10:03 +0200

    Git init, first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('main_picture.png')"
+      },
+      fontFamily: {
+        sans: ['IBM Plex Mono', ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
