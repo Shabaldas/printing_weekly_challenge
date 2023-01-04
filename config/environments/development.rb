9157fe362ecf1cbf98b1d45104a6948ef2a91905
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-01-04 19:49:40 +0200

    Change sing in views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' } # for absolute urls in email
+  config.action_mailer.asset_host = "http://localhost:3000" # for image URLs in HTML email
+
+  # Allow generating absolute urls with routing url helpers.
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
