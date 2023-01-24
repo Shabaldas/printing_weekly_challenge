fee40e46d9e803db44af7d3ae399112117a78980
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-01-24 16:56:45 +0200

    Fix production host

--- Capfile
+++ Capfile
@@ -31,7 +31,7 @@ require "capistrano/rbenv"
 require "capistrano/rails"
 # require "capistrano/chruby"
 # require "capistrano/bundler"
-# require "capistrano/rails/assets"
+require "capistrano/rails/assets"
 # require "capistrano/rails/migrations"
 require "capistrano/passenger"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "https://printingweekly.com"
+  # config.asset_host = "https://printingweekly.com"
   # config.action_mailer.default_url_options = { host: 'printingweekly.com' }
 
   # Specifies the header that your server uses for sending files.
